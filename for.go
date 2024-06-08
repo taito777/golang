4db7bd04901736d11a396de659f720eb56c5a25e
taito <taito@local.com>
2024-06-08 23:54:23 +0900

    2024/06/08

--- for.go
+++ for.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	alias "fmt" // パッケージにエイリアスを設定することが可能
 )
 
 type A struct {
@@ -12,25 +12,22 @@ type A struct {
 
 func (r *A) ChangeName(name string) {
 	r.Name = name
-	fmt.Println(*r)
+	alias.Println(*r)
 }
 
 func main() {
-	//c := A{55, ""}
-	//fmt.Println(c)
-	//r := &c
-
-	//flag.Parse()
-	//r.ChangeName(flag.Arg(0))
+	b := A{33, ""}       // A structを初期化した値を変数bに入力
+	c := &b              // 変数cに変数bのメモリアドレスを入力
+	c.ChangeName("tabi") // ChangeName関数の使用
 
 	for i := 0; i <= 5; i++ {
-		flag.Parse()
-		a := A{i, flag.Arg(i)}
+		flag.Parse()           // flag.Parse(): 引数をコード内で取得
+		a := A{i, flag.Arg(i)} // flag.Arg(i): i番目の引数
 
 		if a.ID <= 3 && a.Name != "" {
-			fmt.Println(a)
+			alias.Println(a)
 		} else {
-			fmt.Println("error")
+			alias.Println("error")
 		}
 
 	}
