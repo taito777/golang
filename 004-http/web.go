8a6771bdb684b9cd3f6a13c05fac948d1a371f09
taito <taito@local.com>
2024-06-10 22:18:16 +0900

    2024/06/10

--- 004-http/web.go
+++ 004-http/web.go
@@ -5,19 +5,25 @@ import (
 	"net/http"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, World!")
-}
+func Handler(w http.ResponseWriter, r *http.Request) {
+	// http.ResponseWriter:リクエストに対してのレスポンスを格納
+	// http.Request:サーバへのhttpリクエストを格納
 
-func goodHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Good, World!")
+	fmt.Fprintln(w, "Hello, World!")
+	// Fprintln(書き込み先, 書き込み内容)
 }
 
 func main() {
-	http.HandleFunc("/hello/", helloHandler) // HandleFunc:指定したパスへのリクエストを処理するメソッドを指定
-	http.HandleFunc("/good/", goodHandler)   // http.HundleFunc("パス", "メソッド")
+	http.HandleFunc("/hello/", Handler)
+	// http.HandleFunc:指定したパスへのリクエストを処理するメソッドを指定
+	// http.HundleFunc("パス", "メソッド")
+
 	fmt.Println("Starting server at port 8080")
+
 	if err := http.ListenAndServe(":8080", nil); err != nil {
+		// http.ListenAndServe(addr string, handler Handler) error:指定したアドレスでリクエストを待つようになる
+		// errorが発生した場合、エラー内容を戻り値として返す
+
 		fmt.Println("Error starting server:", err)
 	}
 }
